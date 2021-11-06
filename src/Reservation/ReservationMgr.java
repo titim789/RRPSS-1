bff724501c64b72c115aac785f710ee221918a1d
NgZhiKai <78411240+NgZhiKai@users.noreply.github.com>
2021-11-06 20:54:21 +0800

    Update ReservationMgr.java

--- src/Reservation/ReservationMgr.java
+++ src/Reservation/ReservationMgr.java
@@ -80,7 +80,7 @@ public class ReservationMgr {
 	public void removeReservationTime() {
 		
 		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.MINUTE, -5);
+		cal.add(Calendar.MINUTE, -15);
 		
 		try {
 		int i;
