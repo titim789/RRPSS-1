378fa8a5bb13e2ab2dfb9b4ff31aed3e61f4fb04
NgZhiKai <78411240+NgZhiKai@users.noreply.github.com>
2021-11-12 08:20:56 +0800

    Update ReservationMgr.java

--- src/reservation/ReservationMgr.java
+++ src/reservation/ReservationMgr.java
@@ -39,7 +39,12 @@ public class ReservationMgr {
 	
 	//-----------------Check Availability---------------------------//
 	public void checkAvail(int n){
-		tableManager.checkAvailability(n);
+		if(tableManager.checkAvailability(n)) {
+			tableManager.displaySizeVacant(n);
+		}
+		else{
+			System.out.println("No table of Size "+ n + " is vacant!!");
+		}
 	}
 	
 	//-----------------Cusomter arrive delete reservation change to occupied---------------------------//
