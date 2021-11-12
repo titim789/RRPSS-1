03d2b7f1c3cb91a833f70eb527812af1463e46ce
Trev <trevorlouislim@gmail.com>
2021-11-12 17:34:29 +0800

    Ensure ID exists

---
+++ src/order/OrderUI.java
@@ -0,0 +1,18 @@
+package order;
+import java.util.Scanner;
+
+public class OrderUI {
+	public OrderUI() {
+		
+	}
+	
+	public int getOrderId() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\nEnter order ID: ");
+		int orderId = sc.nextInt();
+		sc.close();
+		return orderId;
+	}
+	
+
+}
